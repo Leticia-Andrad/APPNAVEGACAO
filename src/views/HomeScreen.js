cdce3fcc4a1ab921828d40481322b924d769d629
Leticia <leticiacarol3622@gmail.com>
2024-06-13 09:28:05 -0300

    adiciona o paper ao projeto

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,20 +1,36 @@
-import {View, Text, Button } from "react-native";
+import { View, } from "react-native";
+import { Card, Text, Button } from 'react-native-paper';
+
 
 export default function HomeScreen(props) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title='Ir para Sobre'
-          onPress={() => props.navigation.navigate('Sobre')}/>
-  
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
       <Button
-          title='Ir para Produtos'
-          onPress={() => props.navigation.navigate('Produtos')}/>
-  
+        title='Ir para Sobre'
+        onPress={() => props.navigation.navigate('Sobre')} />
+
       <Button
-          title='Ir para Contato'
-          onPress={() => props.navigation.navigate('Contato')}/>
-      </View>
-    );
-  }
+        title='Ir para Produtos'
+        onPress={() => props.navigation.navigate('Produtos')} />
+
+      <Button
+        title='Ir para Contato'
+        onPress={() => props.navigation.navigate('Contato')} />
+
+      <Card>
+        <Card.Title title="Card Title" subtitle="Card Subtitle" />
+        <Card.Content>
+          <Text variant="titleLarge">Card title</Text>
+          <Text variant="bodyMedium">Card content</Text>
+        </Card.Content>
+        <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+        <Card.Actions>
+          <Button>Cancel</Button>
+          <Button>Ok</Button>
+        </Card.Actions>
+      </Card>
+
+    </View>
+  );
+}
